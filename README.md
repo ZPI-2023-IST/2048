893b883c62130e2a36865c9fb53d467e288875d5
Mateusz <mateusz.wozniak.0211@gmail.com>
2023-11-27 18:43:42 +0100

    Readme changes and docstrings in 2048.py

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # 2048
+
+Implementation of 2048 game as a module used for final engineering project.
+
+## Game representation
+
+Game board is represented by **r * c** matrix, where **r** indicates number of rows and **c** stands for columns. Each cell contains a **Node** object whose *value* field contains either an integer or None when the cell should be empty.
+
+## API
+
+When using this module you can use following functions:
+
+1. get_moves() -> list:
+
+    Provides possible moves as a list of *w/s/a/d* characters meaning *up/down/left/right* respectively
+
+2. make_move(move: tuple) -> bool:
+
+    Returns True if move succeeded, False otherwise.
+
+    Requires *move* in form of one element tuple, containing character mentioned above.
+
+    Example: make_move('w',) will perform an upwards move.
+
+3. get_state() -> State:
+
+    Returns game state enum:  State.{*ONGOING* / *WON* / *LOST*}.
+
+4. get_board() -> list:
+
+    Returns current board state as a list of lists (rows).
+
+5. start_game() -> None:
+
+    Overwrites current object, invoking constructor with default values and resetting every variable.
--- code2048/2048.py
+++ code2048/2048.py
@@ -7,19 +7,39 @@ class game2048(Game):
         self.board = board if board else Board(rows, cols)
 
     def get_moves(self) -> list:
+        """
+        Provides possible moves as a list of w/s/a/d characters meaning up/down/left/right respectively
+        """
         return list(self.board.possible_moves.values())
 
     def make_move(self, move: tuple) -> bool:
+        """
+        Returns True if move succeeded, False otherwise.
+
+        Requires move in form of one element tuple, containing character mentioned above.
+
+        Example: make_move('w',) will perform an upwards move.
+        """
+
         if move[0] in self.get_moves():
             self.board.make_move(move)
             return True
         return False
 
     def get_state(self) -> State:
+        """
+        Returns game state enum:  State.{ONGOING / WON / LOST}.
+        """
         return self.board.game_status()
 
     def get_board(self) -> list:
+        """
+        Returns current board state as a list of lists (rows).
+        """
         return self.board.board
 
     def start_game(self) -> None:
+        """
+        Overwrites current object, invoking constructor with default values and resetting every variable.
+        """
         self.board = Board()
